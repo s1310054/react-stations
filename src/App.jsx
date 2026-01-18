200c3b12ab88dc792b8f83165473e498a8c09a3a
s1310054 <s1310054@u-aizu.ac.jp>
2026-01-18 22:23:21 +0900

    Update App.jsx
    コード更新日  2025/12/23

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-// DO NOT DELETE
 
 import './App.css'
+// import {useState} from 'react'
+import { Header } from './Header'
+import { Description } from './Description'
+import { DogListContainer } from './DogListContainer'
+import { DogImage } from './DogImage'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  console.log('You clicked update.');
+  <DogImage/>
   return (
-    <div>
-      <h2>Hello, world!</h2>
+    <div className='backGround'>
+      {/* ヘッダー部分（アプリタイトル） */}
+      <Header />
+
+      {/* アプリの簡単な説明とランダム画像表示ボタン */}
+      <Description />
+
+      {/* 犬種を選んでリスト表示する部分 */}
+      <DogListContainer/>
     </div>
-  )
-}
+  );
+};
+
+export default App;
