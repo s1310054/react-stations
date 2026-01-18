d3f317ffd8f5c1381e50f1b1130b973188bef296
s1310054 <s1310054@u-aizu.ac.jp>
2026-01-18 22:29:01 +0900

    Update BreedsSelect.jsx
    コード更新日  2025/11/09

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,24 @@
-// @ts-check
 
-export const BreedsSelect = () => {
-  return <></>
-}
+import React from "react"
+import { useEffect } from "react"
 
-export default BreedsSelect
+/**
+ * props:
+ * - breeds: 犬種リスト（配列）
+ * - selectedBreed: 現在選択中の犬種
+ * - onBreedChange: 犬種変更時に呼ばれる関数
+ */
+export const BreedsSelect = ({ breeds, selectedBreeds, onBreedChange }) => {
+  return (
+    <select value={selectedBreeds} onChange={onBreedChange}>
+      {/* 犬種の一覧を <option> 要素として展開 */}
+      {breeds.map((breed) => (
+        <option key={breed} value={breed}>
+          {breed}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default BreedsSelect;
